584395257511d63d9fb8e1fc8cd3a24f0789cede
dksokoiv <soren.iversen@lego.com>
2025-03-21 20:34:24 +0100

    added docs

--- api/main.py
+++ api/main.py
@@ -3,10 +3,10 @@ from fastapi.staticfiles import StaticFiles
 from routers import (auth_router)
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(docs_url="/docs", redoc_url=None, openapi_url=None)
+app = FastAPI(docs_url="/docs", redoc_url=None, openapi_url="/openapi.json")
 
-# Serve the entire React build at the root
-app.mount("/", StaticFiles(directory="../ui/build", html=True), name="static")
+# Serve the entire React build at the root path /ui
+app.mount("/ui", StaticFiles(directory="../ui/build", html=True), name="static")
 @app.get("/api")
 async def read_api():
     return {"message": "Hello, API with Poetry and React!"}
