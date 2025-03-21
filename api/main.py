f95d5fc589a4ac00a10c2f225c570a870adc1879
dksokoiv <soren.iversen@lego.com>
2025-03-22 00:34:48 +0100

    paths

--- api/main.py
+++ api/main.py
@@ -20,11 +20,10 @@ app.add_middleware(
     allow_headers=["*","Authorization"],  # Allow Authorization header
 )
 # Serve the entire React build at the root path /ui
-app.mount("/ui", StaticFiles(directory="../ui/build", html=True), name="static")
 
 @app.exception_handler(404)
 async def not_found(request: Request, exc: Exception):
     # Return the index.html (React's entry point) when route is not found
     return FileResponse(os.path.join("../ui/build", "index.html"))
 
-app.include_router(auth_router.router, prefix="/api")
+app.include_router(auth_router.router)
