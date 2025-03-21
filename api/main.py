d77cc4828f2461aeec0f131dbc2e3a36b4eddb7f
dksokoiv <soren.iversen@lego.com>
2025-03-21 20:42:18 +0100

    added docs

--- api/main.py
+++ api/main.py
@@ -5,13 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(docs_url="/docs", redoc_url=None, openapi_url="/openapi.json")
 
-# Serve the entire React build at the root path /ui
-app.mount("/ui", StaticFiles(directory="../ui/build", html=True), name="static")
-@app.get("/api")
-async def read_api():
-    return {"message": "Hello, API with Poetry and React!"}
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "https://sorenkoelbaek.dk","http://localhost:8000"],
@@ -19,5 +12,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Serve the entire React build at the root path /ui
+app.mount("/ui", StaticFiles(directory="../ui/build", html=True), name="static")
+@app.get("/api")
+async def read_api():
+    return {"message": "Hello, API with Poetry and React!"}
+
+
 
 app.include_router(auth_router.router)
