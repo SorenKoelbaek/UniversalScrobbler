dd64e50b4017062634bbc3a94c65684aeebab6c2
dksokoiv <soren.iversen@lego.com>
2025-03-21 23:49:21 +0100

    paths

--- api/main.py
+++ api/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from routers import (auth_router)
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+import os
 
 app = FastAPI(docs_url="/docs", redoc_url=None, openapi_url="/openapi.json")
 
@@ -17,11 +20,10 @@ app.add_middleware(
     allow_headers=["*","Authorization"],  # Allow Authorization header
 )
 # Serve the entire React build at the root path /ui
-app.mount("/ui", StaticFiles(directory="../ui/build", html=True), name="static")
-@app.get("/api")
-async def read_api():
-    return {"message": "Hello, API with Poetry and React!"}
+app.mount("/", StaticFiles(directory="../ui/build", html=True), name="static")
 
+@app.exception_handler(404)
+async def not_found(request: Request, exc: Exception):
+    # Return the index.html (React's entry point) when route is not found
+    return FileResponse(os.path.join("../ui/build", "index.html"))
 
-
-app.include_router(auth_router.router)
